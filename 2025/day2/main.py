b999b87d279646636e09eef103753a6b60dc9dc4
Torsten Mangner <tmangner@inoxio.de>
2025-12-02 08:55:50 +0100

    Day2 2025

---
+++ 2025/day2/main.py
@@ -0,0 +1,52 @@
+def solve_a(id_ranges):
+    invalid = 0
+    for id_range in id_ranges:
+        start, end = id_range.split("-")
+        start = int(start)
+        end = int(end)
+
+        for id in range(start, end + 1):
+            if is_invalid_a(str(id)):
+                invalid += id
+    return invalid
+
+
+def is_invalid_a(part):
+    length = len(part)
+    first_half = part[: length // 2]
+    last_half = part[length // 2 :]
+    return first_half == last_half
+
+
+def solve_b(id_ranges):
+    invalid = 0
+    for id_range in id_ranges:
+        start, end = id_range.split("-")
+        start = int(start)
+        end = int(end)
+
+        for id in range(start, end + 1):
+            if is_invalid_b(str(id)):
+                invalid += id
+    return invalid
+
+
+def is_invalid_b(part):
+    for chunk_length in range(len(part) // 2, 0, -1):
+        if (len(part) % chunk_length) != 0:
+            continue
+
+        if inner(part, chunk_length):
+            return True
+
+    return False
+
+
+def inner(part, chunk_length):
+    first_chunk = part[:chunk_length]
+    for i in range(chunk_length, len(part), chunk_length):
+        if part[i : i + chunk_length] != first_chunk:
+            print("mismatch at", i)
+            return False
+
+    return True
---
+++ 2025/day2/test_main.py
@@ -0,0 +1,35 @@
+from main import is_invalid_a, is_invalid_b, solve_a, solve_b
+
+example_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+
+def test_invalid_a():
+    assert is_invalid_a("11") is True
+    assert is_invalid_a("1188511885") is True
+    assert is_invalid_a("222222") is True
+
+
+def test_solve_a_example():
+    assert solve_a(example_input.split(",")) == 1227775554
+
+
+def test_solve_a():
+    assert solve_a(open("2025/day2/input.txt").read().strip().split(",")) == 24043483400
+
+
+def test_invalid_b():
+    assert is_invalid_b("123123") is True
+    assert is_invalid_b("38593859") is True
+    assert is_invalid_b("21212121") is True
+    assert is_invalid_b("11111") is True
+
+    assert is_invalid_b("111112") is False
+    assert is_invalid_b("123124") is False
+
+
+def test_solve_b_example():
+    assert solve_b(example_input.split(",")) == 4174379265
+
+
+def test_solve_b():
+    assert solve_b(open("2025/day2/input.txt").read().strip().split(",")) == 38262920235
